e836fb8af2054c74649a710eea33c18a2fe3a699
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2024-07-04 12:48:07 +0300

    Made "qpb_gamma5_sign_function" publicly accessible

--- lib/qpb_overlap_kl.h
+++ lib/qpb_overlap_kl.h
@@ -8,6 +8,10 @@ void qpb_overlap_kl_init(void *, qpb_clover_term, qpb_double, qpb_double,\
                                                         qpb_double, qpb_double);
 void qpb_overlap_kl_finalize();
 
+void qpb_gamma5_sign_function_partial_fractions(qpb_spinor_field,\
+    qpb_spinor_field, qpb_complex, qpb_double*,\
+    qpb_double*, int, qpb_double, int);
+
 void qpb_overlap_kl(qpb_spinor_field, qpb_spinor_field, enum qpb_kl_classes,\
                                                         int, qpb_double, int);
 void qpb_gamma5_overlap_kl(qpb_spinor_field, qpb_spinor_field,
