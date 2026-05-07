3224c4de614a0338549068a62d0ea95126ad21ee
Stylianos Gregoriou <s.gregoriou@cyi.ac.cy>
2026-05-07 22:14:21 +0300

    Change position of preconditioner message

--- lib/qpb_overlap_kl_pfrac.c
+++ lib/qpb_overlap_kl_pfrac.c
@@ -587,12 +587,13 @@ qpb_preconditioner_bicgstab(qpb_spinor_field x, qpb_spinor_field b)
     qpb_spinor_xdotx(&res_norm, r);
   }
 
-  if(iters == prec_CG_max_iter)
-    return -1;
-
   print(" \tAfter %d iters, preconditioner solver converged, " \
     "relative res = %e, relative = %e, t = %g sec\n", \
     iters, res_norm / bprime_norm);
+    
+  if(iters == prec_CG_max_iter)
+    return -1;
+
 
   return iters;
 }
